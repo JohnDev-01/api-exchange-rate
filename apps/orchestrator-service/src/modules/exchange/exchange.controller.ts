ef855498a0904582a7ad4e8639237eeb2c2a4e6c
John Silvestre <johnkerlin52@gmail.com>
2025-08-03 14:53:03 -0400

    feat(exchange): add bestExchange endpoint with query params and logging

--- apps/orchestrator-service/src/modules/exchange/exchange.controller.ts
+++ apps/orchestrator-service/src/modules/exchange/exchange.controller.ts
@@ -1,16 +1,17 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Logger, Query } from '@nestjs/common';
 import { ExchangeService } from './exchange.service';
 
 @Controller('exchange')
 export class ExchangeController {
   constructor(private readonly exchangeService: ExchangeService) {}
 
-  @Get()
+  @Get('bestExchange')
   async getBestExchange(
     @Query('sourceCurrency') sourceCurrency: string,
     @Query('targetCurrency') targetCurrency: string,
     @Query('amount') amount: string,
   ) {
+    Logger.log('Call bestExchange endpoint');
     return this.exchangeService.getBestRate(
       sourceCurrency,
       targetCurrency,
